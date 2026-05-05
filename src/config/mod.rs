bdbe6e1ba4ac06200385754a0c6d9d6db7e1733a
mendescotta <mendescotta@gmail.com>
2026-05-05 16:08:42 +0300

    Add files via upload

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,1 +1,10 @@
+use serde::Deserialize;
 
+#[derive(Deserialize)]
+pub struct Config {
+    pub mod_key: Option<String>,
+}
+
+pub fn load() -> Config {
+    Config { mod_key: Some("Alt".into()) }
+}
